5ce51eb7d5a0d4e3d2de37c7ee0b57724fc9a24f
TomyMarengo <marengotomas@gmail.com>
2024-06-05 01:21:08 -0300

    Like the last commit but well done

---
+++ client/src/main/java/ar/edu/itba/pod/client/utils/CsvFileCopier.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.pod.client.utils;
+
+import java.io.*;
+
+public class CsvFileCopier {
+    public static void main(String[] args) {
+        String sourceFilePath = "./ticketsCHI.csv";
+        String destFilePath = "./ticketsCHI2.csv";
+        int linesToCopy = 10000;
+
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(sourceFilePath));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(destFilePath, false))
+        ) {
+            String line = reader.readLine();
+            if (line != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+
+            int lineCount = 0;
+            while ((line = reader.readLine()) != null && lineCount < linesToCopy) {
+                writer.write(line);
+                writer.newLine();
+                lineCount++;
+            }
+
+            System.out.println("Successfully copied " + (lineCount + 1) + " lines (including header).");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
